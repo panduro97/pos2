6119eec63c8e040df10f97e7a840da7582391367
unknown <Desarrollo1@DESARROLLO1.moti.com>
2019-07-06 12:05:51 -0500

    cambios correo

--- controladores/inventarios.controlador.php
+++ controladores/inventarios.controlador.php
@@ -35,6 +35,35 @@ if($bandera == true){
         echo "Error: " . $consulta4 . "<br>" . $conn->error;
     }
 
+
+    $nombre = $email = $subject = $mensaje = $para = $headers = $msjCorreo = NULL;
+
+    /*   if (isset($_POST['submit'])) { */
+         //Obtenemos valores input formulario
+         $nombre = $_POST['nombre'];
+         $email = $_POST['email'];
+         $subject = $_POST['subject'];   
+         $mensaje = $_POST['mensaje'];
+         $para = 'panduro.sergio@outlook.es';
+   
+         //Creamos cabecera.
+         $headers =  'MIME-Version: 1.0' . "\r\n"; 
+         $headers = 'From: Sergio' . "\r\n";
+         $headers .= 'Content-type: text/html;' . "\r\n"; 
+   
+         //Componemos cuerpo correo.
+         $msjCorreo = "Nombre: " . $nombre;
+         $msjCorreo .= "\r\n";
+         $msjCorreo .= "Email: " . $email;
+         $msjCorreo .= "\r\n";
+         $msjCorreo .= "Asunto: " . $subject;
+         $msjCorreo .= "\r\n";
+         $msjCorreo .= "Mensaje: " . $mensaje;
+         $msjCorreo .= "\r\n";
+   
+       mail($para, 'aver', $msjCorreo, $headers)
+     
+
 }
 
 ?>
